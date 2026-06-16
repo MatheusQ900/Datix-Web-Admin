5c4c3d586d9b663c6318e66f56fdde09a76a1e33
MatheusQ900 <quirinomathes19@gmail.com>
2026-06-16 20:42:12 -0300

    painel inicial

---
+++ js/dashboard.js
@@ -0,0 +1,10 @@
+document.getElementById("clientes").innerText = "150";
+document.getElementById("online").innerText = "42";
+document.getElementById("offline").innerText = "108";
+document.getElementById("ultimo").innerText = "PC-001";
+
+document.getElementById("logout").addEventListener("click", () => {
+
+    alert("Logout realizado!");
+
+});
